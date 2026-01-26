dee7ce4083de07b287f44b651a50959bd3b4878c
Danya Panin <panin.danila.2005@yandex.ru>
2026-01-26 22:53:46 +0300

    fixed servo

---
+++ Source code/Steering control/servo.h
@@ -0,0 +1,10 @@
+#ifndef INC_MODULES_SERVO_H_
+#define INC_MODULES_SERVO_H_
+
+#include <stdint.h>
+#include "main.h"
+
+void init_servo();
+void perform_servo_control_step();
+
+#endif /* INC_MODULES_SERVO_H_ */
